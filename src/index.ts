e04c2c4f492df4c28f3bf74f50b14895c1fd623b
garganurag89 <anuraggarg@sharechat.co>
2022-11-17 14:47:58 +0530

    ✨ Added secure express middleware

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+interface SecureOptions {
+  callback?: () => void;
+  handleResponseCustom?: (response: any) => void;
+}
+
 /**
  * Sanitized String for XSS protection
  *
@@ -37,3 +42,37 @@ export const sanitizeUrl = (
   str: string,
   expression: RegExp = /[`~!$%^*()|+;'"<>{}[\]\\]/gi
 ) => (str ? str.replace(expression, '') : str);
+
+/**
+ * Express Middleware for preventing XSS attacks
+ *
+ * @param options - Options for secure middleware
+ *
+ * @returns Middleware handler for xss protection
+ *
+ * @example
+ * ```ts
+ * const sanitizedString = sanitizeUrl("<script>Hello</script>");
+ * const sanitizedString = sanitizeUrl("<script>Hello</script>");
+ * ```
+ */
+export const secure = ({ callback, handleResponseCustom }: SecureOptions) => {
+  return (req: any, res: any, next: any) => {
+    if (
+      /(<|%3C)script[\s\S]*?(>|%3E)[\s\S]*?(<|%3C)(\/|%2F)script[\s\S]*?(>|%3E)/.test(
+        req.url
+      )
+    ) {
+      if (typeof callback === 'function') {
+        callback();
+      }
+      if (typeof handleResponseCustom === 'function') {
+        handleResponseCustom(res);
+      } else {
+        res.status(404).send('Not found! Due to potential security issues');
+      }
+    } else {
+      next();
+    }
+  };
+};
